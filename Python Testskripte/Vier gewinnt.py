5fc516e7f5e8d9301400e4404836a6bec2c16849
gregpackpdm <56831166+gregpackpdm@users.noreply.github.com>
2020-10-02 16:43:48 +0200

    Diagonale_LORU_binaer
    Funktion zur Berechnung der Diagonalen von LO nach RU implementiert
    
    Co-Authored-By: gregpackpdm <56831166+gregpackpdm@users.noreply.github.com>

--- Python Testskripte/Vier gewinnt.py
+++ Python Testskripte/Vier gewinnt.py
@@ -23,6 +23,47 @@ def prüfe_binaere_Zahl(Zahl,Maske):
     print("Ergbnis:" + str(Ergebnis))
     return Ergebnis
 
+def Diagonale_LORU_binaer(SP_LISTE, aktuelle_Zeile, aktuelle_Spalte):
+    #Beispiel 
+    # aktuelle Zeile = 4
+    # aktuell Spalte = 4
+    # Diagonale Z5 = S3 / Z6 = S2 Z7 = S1 - nach LINKS 
+    # Diagonale Z3 = S5 / Z2 = S2 Z1 = S1
+    # SP_LISTE enthält binär codierte Zeile / Reihenfolge Z1->Z7 als Dezimalzahl
+    # Z1 = 65 -> S1 und S7 sind belegt
+
+    Ergebnis = 0
+    # Berechne den Startpunkt der Diagonalen bei Zeile 1
+    Start =1
+    i = 0
+    S = aktuelle_Spalte + (aktuelle_Zeile -1)
+    if S-7 > 0:
+        Start = S-7+1
+        S = min (7,S)
+        
+    
+    for Z in range (Start,8):
+        
+        if not((2 ** (7-S) & SP_LISTE[Z-1]) == 0): #Die Position der Liste ist belegt
+            Ergebnis = Ergebnis + 2 ** i
+         
+        S=S-1
+        i= i +1
+        if S==1:
+            return Ergebnis
+    return Ergebnis
+
+
+
+
+   
+    
+   
+      
+
+
+
+
 
 
 
@@ -93,27 +134,27 @@ def main():
         
         
         current_S = int(Eingabe) -1
-#       Berechner SP1/SP2 LISTE
-#       Die Spielerliste besteht aus einer Dezimalzahl pro Zeile
-#       Jede Dezimalzahl stellt binär die Belegung der Spalten pro Zeile dar
-#       Es gibt 7 Spalten (1..7)
-#       Ist die Spalte 7 belegt ist das erste BIT (2^0=1) = 1
-#       IST die Spalte 6 belegt ist das zweite BIT (2^1=2) = 1
-#       Ist die Spalte 5 belegt ist das dritte BIT (2^2=4) = 1
-#       IST die Spalte 1 belegt ist das siebente BIT (2^7=128) = 1
-#       Durch Addition erhält man die binäre Belegung der Spalten einer Zeile
-#       z. B. 1 0 0 0 0 0 1 [7. und 1. Spalte] = 128 + 1 = 129
-#       der Index der Liste läuft von links Index 0 = Spalte 1 entgegen der binären Codierung
-#       Umrechnung von Spalte auf Index => Spalte (Eingabe) - 1
-#       Umrechnung von Spalte auf Belegungs-Bit => 8 - Spalte (Eingabe) 
+    #       Berechner SP1/SP2 LISTE
+    #       Die Spielerliste besteht aus einer Dezimalzahl pro Zeile
+    #       Jede Dezimalzahl stellt binär die Belegung der Spalten pro Zeile dar
+    #       Es gibt 7 Spalten (1..7)
+    #       Ist die Spalte 7 belegt ist das erste BIT (2^0=1) = 1
+    #       IST die Spalte 6 belegt ist das zweite BIT (2^1=2) = 1
+    #       Ist die Spalte 5 belegt ist das dritte BIT (2^2=4) = 1
+    #       IST die Spalte 1 belegt ist das siebente BIT (2^7=128) = 1
+    #       Durch Addition erhält man die binäre Belegung der Spalten einer Zeile
+    #       z. B. 1 0 0 0 0 0 1 [7. und 1. Spalte] = 128 + 1 = 129
+    #       der Index der Liste läuft von links Index 0 = Spalte 1 entgegen der binären Codierung
+    #       Umrechnung von Spalte auf Index => Spalte (Eingabe) - 1
+    #       Umrechnung von Spalte auf Belegungs-Bit => 7 - Spalte (Eingabe) 
 
 
         if Symbol ==Symbol1:
-            SP1[S_Stand[current_S]] = SP1[S_Stand[current_S]] + 2 ** (8-int(Eingabe))
+            SP1[S_Stand[current_S]] = SP1[S_Stand[current_S]] + 2 ** (7-int(Eingabe))
             Programm_ENDE = prüfe_binaere_Zahl(SP1[S_Stand[current_S]],15) > 0
 
         else:
-            SP2[S_Stand[current_S]] = SP2[S_Stand[current_S]] + 2 ** (8-int(Eingabe))
+            SP2[S_Stand[current_S]] = SP2[S_Stand[current_S]] + 2 ** (7-int(Eingabe))
             Programm_ENDE = prüfe_binaere_Zahl(SP2[S_Stand[current_S]],15) > 0
 
         print("SP1:" + str(SP1))
@@ -131,6 +172,7 @@ def main():
     print ("Spieler :" + Spieler + " hat gewonnen")
     
 
-main()
+TEST_LISTE=[255,255,255,255,255,255,255]
+print(Diagonale_LORU_binaer(TEST_LISTE,4,7))
 
 
